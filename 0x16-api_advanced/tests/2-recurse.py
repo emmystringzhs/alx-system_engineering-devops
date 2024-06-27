ca51313ca9f54dead931540f3f950bd999901ff3
emmystringzhs <milesbetti@gmail.com>
2024-06-27 04:38:05 +0100

    Api advanced

--- 0x16-api_advanced/tests/2-recurse.py
+++ 0x16-api_advanced/tests/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "whytecode.tech"
     }
     params = {
         "after": after,
